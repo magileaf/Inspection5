e5d91d2152b99e63904b75aeadeac0e7a11748a9
Chi Yiu Ku <carrodsk2@hotmail.com>
2022-05-23 05:08:48 +0100

    加入專案檔案。

---
+++ Inspection5/InspectionType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Inspection5
+{
+    public class InspectionType
+    {
+        public int  id { get; set; }
+
+        [StringLength(20)]
+
+        public string InspectionName { get; set; } = String.Empty;
+    }
+}
